760ace530ac1db0d69f2925499528254b79593a9
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-06-29 19:07:07 +0300

    lesson by Alert

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,42 @@
 import Product from './Product';
 import Book from './BookList';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+//       <Product name="Tacos With Lime" price={10.99} />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+//       <Book />;
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import Alert from './Alert';
+import UserMenu from './UserMenu';
+
+export const App = () => {
   return (
-    <div>
-      <h1>Best selling</h1>
-      <Product name="Tacos With Lime" price={10.99} />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-      <Book />;
-    </div>
+    <>
+      <Alert variant="info">
+        <UserMenu /> Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
   );
 };
 
